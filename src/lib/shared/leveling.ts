6d4fe62ee76193574ffa58559e4f91bc5b5a1d90
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-01-06 18:06:35 +1000

    level +33

--- src/lib/shared/leveling.ts
+++ src/lib/shared/leveling.ts
@@ -1,1 +1,1 @@
-export const getXpForNextLevel = (currentLevel: number) => currentLevel ** 2 + currentLevel + 10
+export const getXpForNextLevel = (currentLevel: number) => currentLevel ** 2 + currentLevel + 33
