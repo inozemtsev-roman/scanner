1fcc7dc1445b4c3a5d040a31147fc656642678e6
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-01-22 13:53:37 +1000

    target="_blank" use:ripple

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -93,8 +93,10 @@
 								</p>
 							</div>
 							<div class="item-getgems">
-								<Button variant="primary" href="https://getgems.io/{$t(`items.${itemSelected.id}.link`)}"
-									>{$t('home.links.getgems')}</Button
+								<Button
+									variant="primary"
+									href="https://getgems.io/{$t(`items.${itemSelected.id}.link`)}"
+									target="_blank">{$t('home.links.getgems')}</Button
 								>
 							</div>
 
