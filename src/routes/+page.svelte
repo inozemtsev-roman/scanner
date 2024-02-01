1e91a2ba4ba661f05cbb15b988371ecef5077387
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-02-01 13:11:04 +1000

    gh gg without text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,11 +64,9 @@
 	</a>
 	<a class="nav-card gg" href="https://getgems.io/fingerprints" target="_blank" use:ripple>
 		<img src="/icons/gg.webp" alt="getgems" />
-		{$t('home.links.getgems')}
 	</a>
 	<a class="nav-card gh" href="https://github.com/mir-one/fingerprints" target="_blank" use:ripple>
 		<img src="/icons/github.webp" alt="github" />
-		{$t('home.links.github')}
 	</a>
 </div>
 
