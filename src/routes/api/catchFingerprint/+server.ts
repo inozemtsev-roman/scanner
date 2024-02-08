0bb4411932a297242c202d5d5f3964e87e78c02e
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-02-08 11:53:26 +1000

    waitMinTime after drop

--- src/routes/api/catchFingerprint/+server.ts
+++ src/routes/api/catchFingerprint/+server.ts
@@ -7,7 +7,7 @@ import { giveUserItem, giveUserXp } from '$lib/server/user'
 const xpReward = 1
 const catchPrice = 3
 const cooldown = 10_000
-const waitMinTime = 6_000
+const waitMinTime = 15_000
 
 export async function POST(event: RequestEvent) {
 	const { t, user, initData } = event.locals
