3cee38104260035e46f4740d6231108f8b5bf0ae
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-02-14 15:33:24 +1000

    rgba(var(--background),0.33)

--- src/lib/client/components/Drawer.svelte
+++ src/lib/client/components/Drawer.svelte
@@ -42,7 +42,7 @@
 <style lang="scss">
 	.drawer {
 		min-height: 18rem;
-		background: rgba(var(--background), 0.33);
+		background: var(--background);
 		width: 100%;
 		max-width: initial;
 		max-height: 32rem;
